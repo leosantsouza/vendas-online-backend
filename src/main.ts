6db5f6478a89d4478ec4c2d3018851576b91cfc1
leosantsouza <leosantsouza@hotmail.com>
2023-05-03 22:58:39 -0300

    teste commit

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
-  console.log( "Online in http://localhost:8080");
+  await app.listen(3000);
+  console.log('Online in http://localhost:3000');
 }
-bootstrap();
 
+bootstrap();
